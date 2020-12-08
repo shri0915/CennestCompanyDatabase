71737d5af95e62a4f49575cdbf71886be24e2b72
shri0915 <shrijit.panicker@gmail.com>
2020-12-08 17:28:15 +0530

    Added and mapped ProjectTask table

---
+++ Company/ProjectTask.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ProjectTask]
+(
+	[ProjectTaskID] INT NOT NULL PRIMARY KEY, 
+    [ProjectID] INT NOT NULL, 
+    [TaskID] INT NOT NULL, 
+    [ProjectTaskStatus] INT NULL, 
+    CONSTRAINT [FK_ProjectTask_Project] FOREIGN KEY ([ProjectID]) REFERENCES [Project]([ProjectID]), 
+    CONSTRAINT [FK_ProjectTask_Task] FOREIGN KEY ([TaskID]) REFERENCES [Task]([TaskID]), 
+    CONSTRAINT [FK_ProjectTask_StatusMaster] FOREIGN KEY ([ProjectTaskStatus]) REFERENCES [StatusMaster]([StatusID])
+)
