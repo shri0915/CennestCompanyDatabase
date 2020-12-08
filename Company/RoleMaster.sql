a4cfb4aa4d7e11767f6f9f5327f76f0872d9eef5
shri0915 <shrijit.panicker@gmail.com>
2020-12-08 17:14:36 +0530

    Added Role and Status master tables

---
+++ Company/RoleMaster.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[RoleMaster]
+(
+	[RoleID] INT NOT NULL PRIMARY KEY, 
+    [RoleName] TEXT NOT NULL, 
+    [RoleDescription] TEXT NULL
+)
---
+++ Company/StatusMaster.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[StatusMaster]
+(
+	[StatusID] INT NOT NULL PRIMARY KEY, 
+    [StatusName] TEXT NOT NULL, 
+    [StatusDescription] TEXT NULL
+)
