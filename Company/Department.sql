fd1543fbb39f71d62ec63c1e7c51e02bf790722c
shri0915 <shrijit.panicker@gmail.com>
2020-12-08 16:51:20 +0530

    Add project files.

---
+++ Company/Department.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Department]
+(
+	[DepartmentID] INT NOT NULL PRIMARY KEY, 
+    [DepartmentName] TEXT NULL
+)
---
+++ Company/Employee.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[EmployeeID] INT NOT NULL PRIMARY KEY, 
+    [EmployeeName] TEXT NULL, 
+    [DepartmentID] INT NULL, 
+    CONSTRAINT [FK_Employee_Department] FOREIGN KEY ([DepartmentID]) REFERENCES [Department]([DepartmentID])
+)
