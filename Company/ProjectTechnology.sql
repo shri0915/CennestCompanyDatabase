042f6de52db3b687e979e17a82645573b9522985
shri0915 <shrijit.panicker@gmail.com>
2020-12-08 17:35:46 +0530

    Added and mapped Project Technology table

---
+++ Company/ProjectTechnology.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ProjectTechnology]
+(
+	[ProjectTechnologyID] INT NOT NULL PRIMARY KEY, 
+    [ProjectID] INT NOT NULL, 
+    [TechnologyID] INT NOT NULL, 
+    [ProjectTechnologyVersionNumber] TEXT NOT NULL, 
+    CONSTRAINT [FK_ProjectTechnology_Project] FOREIGN KEY ([ProjectID]) REFERENCES [Project]([ProjectID]), 
+    CONSTRAINT [FK_ProjectTechnology_Technology] FOREIGN KEY ([TechnologyID]) REFERENCES [Technology]([TechnologyID])
+)
