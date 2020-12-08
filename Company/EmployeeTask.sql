de0b1da9ea5c97629248c0f2f3517700b504e216
shri0915 <shrijit.panicker@gmail.com>
2020-12-08 17:23:28 +0530

    Added and mapped EmployeeTask table

---
+++ Company/EmployeeTask.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[EmployeeTask]
+(
+	[EmployeeTaskID] INT NOT NULL PRIMARY KEY, 
+    [EmployeeID] INT NOT NULL, 
+    [TaskID] INT NOT NULL, 
+    [EmployeeTaskStatus] INT NULL, 
+    CONSTRAINT [FK_EmployeeTask_Employee] FOREIGN KEY ([EmployeeID]) REFERENCES [Employee]([EmployeeID]), 
+    CONSTRAINT [FK_EmployeeTask_Task] FOREIGN KEY ([TaskID]) REFERENCES [Task]([TaskID]), 
+    CONSTRAINT [FK_EmployeeTask_StatusMaster] FOREIGN KEY ([EmployeeTaskStatus]) REFERENCES [StatusMaster]([StatusID])
+)
--- Company/Project.sql
+++ Company/Project.sql
@@ -6,5 +6,6 @@
     [ClientID] INT NOT NULL, 
     [DepartmentID] INT NOT NULL, 
     CONSTRAINT [FK_Project_Client] FOREIGN KEY ([ClientID]) REFERENCES [Client]([ClientID]), 
-    CONSTRAINT [FK_Project_Department] FOREIGN KEY ([DepartmentID]) REFERENCES [Department]([DepartmentID])
+    CONSTRAINT [FK_Project_Department] FOREIGN KEY ([DepartmentID]) REFERENCES [Department]([DepartmentID]), 
+    CONSTRAINT [FK_Project_StatusMaster] FOREIGN KEY ([ProjectStatus]) REFERENCES [StatusMaster]([StatusID])
 )
