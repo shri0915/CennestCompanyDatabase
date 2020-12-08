0c38fcb44a268090f1f409c9966a68a3f6617a95
shri0915 <shrijit.panicker@gmail.com>
2020-12-08 17:39:25 +0530

    Added and mapped EmployeeProject table

---
+++ Company/EmployeeProject.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[EmployeeProject]
+(
+	[EmployeeProjectID] INT NOT NULL PRIMARY KEY, 
+    [EmployeeID] INT NOT NULL, 
+    [ProjectID] INT NOT NULL, 
+    [EmployeeRoleID] INT NOT NULL, 
+    CONSTRAINT [FK_EmployeeProject_Employee] FOREIGN KEY ([EmployeeID]) REFERENCES [Employee]([EmployeeID]), 
+    CONSTRAINT [FK_EmployeeProject_Project] FOREIGN KEY ([ProjectID]) REFERENCES [Project]([ProjectID]), 
+    CONSTRAINT [FK_EmployeeProject_RoleMaster] FOREIGN KEY ([EmployeeRoleID]) REFERENCES [RoleMaster]([RoleID])
+)
