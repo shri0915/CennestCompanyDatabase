ba6b198b2b5dbd81362183b80b2ed0d68446017f
shri0915 <shrijit.panicker@gmail.com>
2020-12-08 17:31:54 +0530

    Added and Mapped TaskTechnology table

---
+++ Company/TaskTechnology.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TaskTechnology]
+(
+	[TaskTechnologyID] INT NOT NULL PRIMARY KEY, 
+    [TaskID] INT NOT NULL, 
+    [TechnologyID] INT NOT NULL, 
+    CONSTRAINT [FK_TaskTechnology_Task] FOREIGN KEY ([TaskID]) REFERENCES [Task]([TaskID]), 
+    CONSTRAINT [FK_TaskTechnology_Technology] FOREIGN KEY ([TechnologyID]) REFERENCES [Technology]([TechnologyID])
+)
